70712cf55075ad80be51e887becaf9056a3b20ee
anna099 <annahart099@gmail.com>
2019-03-22 16:42:13 +0000

    commenting and name changes for binary

--- binary.hs
+++ binary.hs
@@ -1,5 +1,8 @@
+-- converts a binary number, represented as a list
+-- (e.g. [1,0,1,1,0,1]) to a decimal number.
+
 values :: Int -> [Int]
 values len = reverse (take len [2 ^ x | x <- [0..len]])
 
 bin2dec :: [Int] -> Int
-bin2dec digits = sum (zipWith (*) digits (values (length digits)))
+bin2dec xs = sum (zipWith (*) xs (values (length xs)))
